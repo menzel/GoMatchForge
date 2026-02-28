fa6518a815129c18d089cf43b4d22003cb286d60
menzel <michael.menzel@med.uni-heidelberg.de>
2026-02-28 11:18:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -296,7 +296,7 @@ def prev_plays(p1: str, p2: str, hist: pd.DataFrame) -> int:
 
 def penalty(p1d: dict, p2d: dict, hist: pd.DataFrame) -> int:
     tz = abs(p1d["timezone"] - p2d["timezone"])
-    tz = tz*p1d['timezone_matters']*p2d['timezone_matters']
+    tz = tz*1#p1d['timezone_matters']*p2d['timezone_matters']
     tp = 8 if tz>10 else (3 if tz>4 else 0)
     rp = abs(rank_to_int(p1d,hist) - rank_to_int(p2d,hist))
     rp = rp*2 if tz>=5 else rp
